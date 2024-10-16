44055ead5181476ab8085b0ab72a16871f0a214f
Noxian <nixaris@gmail.com>
2024-10-16 03:57:07 +0300

    pep8 fix

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -6,4 +6,3 @@ class IsAutorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(
             request.method in SAFE_METHODS or request.user == obj.author)
-
