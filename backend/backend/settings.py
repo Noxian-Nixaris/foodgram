3ccbe9efea9c766a561edeeb6cb24640d2aa53f2
Noxian <nixaris@gmail.com>
2024-10-19 19:10:46 +0300

    0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,7 +62,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
