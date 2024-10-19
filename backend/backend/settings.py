9114e773d3fc0ef486bb1571bebfc50221d83e9a
Noxian <nixaris@gmail.com>
2024-10-19 19:05:56 +0300

    Test 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,6 +62,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
